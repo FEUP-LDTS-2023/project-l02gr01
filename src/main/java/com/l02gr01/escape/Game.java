dbc81b630b327ba732dd6f49b420a7c00886f41a
Filipe Correia <68955432+fmcorreia04@users.noreply.github.com>
2023-11-19 18:59:40 +0000

    Added barebones code for GUI and game initialization

--- src/main/java/com/l02gr01/escape/Game.java
+++ src/main/java/com/l02gr01/escape/Game.java
@@ -1,5 +1,26 @@
 package com.l02gr01.escape;
 
+import gui.LanternaGUI;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URISyntaxException;
+
 public class Game {
+    private final LanternaGUI gui;
+
+    public Game() throws IOException, URISyntaxException, FontFormatException {
+        // Add singleton logic
+        this.gui = new LanternaGUI(20, 20);
+
+
+    }
+
+    public static void main(String[] args) throws IOException, FontFormatException, URISyntaxException {
+        new Game().start();
+    }
+    private void start() throws IOException {
+        // start the gui
+    }
 
 }
---
+++ src/main/java/gui/GUI.java
@@ -0,0 +1,15 @@
+package gui;
+
+import java.io.IOException;
+
+public interface GUI {
+    ACTION getNextAction() throws IOException;
+
+    void clear();
+
+    void refresh() throws IOException;
+
+    void close() throws IOException;
+
+    enum ACTION {UP, RIGHT, DOWN, LEFT, NONE, QUIT, SELECT}
+}
