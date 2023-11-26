1690bdc74f034c04599635c0290457c298e50c38
Filipe Correia <68955432+fmcorreia04@users.noreply.github.com>
2023-11-26 15:05:37 +0000

    Minor correction: add final

--- src/main/java/com/l02gr01/escape/model/history/History.java
+++ src/main/java/com/l02gr01/escape/model/history/History.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class History {
-    private static History instance = new History();
+    private static final History instance = new History();
     private List<Event> history;
 
 
